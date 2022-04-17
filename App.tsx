ad613e50a52cda1c386185171a379a91fd11a3d5
Ravil Safargaliev <r.safargaliev@andersenlab.com>
2022-04-17 14:58:46 +0300

    created a Chat template with react-native-gifted-chat

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AppChat from './src/AppChat/AppChat';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppChat />
     </View>
   );
 }
@@ -13,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
